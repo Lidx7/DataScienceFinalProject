ab44c4c6139f49f9519139c0d0bd1b86beb3e845
lidor <lidor.borofker@windowslive.com>
2024-03-05 16:49:13 +0200

    get csv

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from BayesClassifier import BayesClassifier
 from KNNClassifier import KNNClassifier
 import pandas as pd
-
-def main():
+def BayesPrinter():
     # Running BayesClassifier
     bayes_classifier = BayesClassifier()
     bayes_classifier.evaluate_train_and_test()
-
+def KNNPrinter():
     # Running KNNClassifier
     knn_classifier = KNNClassifier(k=5)
     data = pd.read_csv('spam_dataset.csv')
@@ -15,6 +14,9 @@ def main():
     print("Bayes")
     knn_classifier.fit(X, y)
     knn_classifier.predict()
+def main():
+    BayesPrinter()
+    KNNPrinter()
 
 if __name__ == "__main__":
     main()
